b8966cad40485902896ef33bf429b6624e7a45fd
gaba-teixeira <gaba-teixeira@users.noreply.github.com>
2023-12-06 11:08:45 -0800

    Moved the project inside the folder

---
+++ src/index.js
@@ -0,0 +1,48 @@
+function changeCity(event) {
+  event.preventDefault();
+  let cityValue = document.querySelector("#city-imput");
+  let head = document.querySelector(`h1`);
+  head.innerHTML = `${cityValue.value}`;
+  let apiKey = "ab14t46ca824c44f19c53892o77f5c04";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityValue.value}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature-value");
+
+  temperatureElement.innerHTML = `${temperature}`;
+}
+
+function changeTime(date) {
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = days[date.getDay()];
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  return `${day}, ${hour}:${minutes}`;
+}
+
+let city = document.querySelector("#city-form");
+city.addEventListener("submit", changeCity);
+
+let time = document.querySelector("#current-time");
+let currentTime = new Date();
+
+time.innerHTML = changeTime(currentTime);
